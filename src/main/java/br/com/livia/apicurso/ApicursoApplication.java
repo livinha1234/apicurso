3e1519065d48429a76f70e54f0da3597fd50b374
unknown <liviamariasouza268@gmail.com>
2022-06-06 08:20:30 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/livia/apicurso/ApicursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.livia.apicurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicursoApplication.class, args);
+	}
+
+}
